3abe356fbd453db82bc8178bad2d172c83549e7f
PeterZhang2015 <zhangchongzheng@gmail.com>
2025-05-08 16:29:55 +1000

    Update module variable.

--- TestTerraformProject1/environments/dev/main.tf
+++ TestTerraformProject1/environments/dev/main.tf
@@ -1,6 +1,6 @@
 module "webserver" {
   source        = "..//../modules/webserver"
-  ami           = var.ami_id
+  ami_id           = var.ami_id
   instance_type = var.instance_type
   instance_tags = var.instance_tags
 }
